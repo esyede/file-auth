5681eabad238b3d78661eee4309e188bb5823c10
esyede <suyadi.1992@gmail.com>
2021-12-10 05:27:20 +0700

    small changes

--- boot.php
+++ boot.php
@@ -2,10 +2,10 @@
 
 defined('DS') or exit('No direct script access.');
 
-Autoloader::map([
+System\Autoloader::map([
     'Esyede\FileAuth' => __DIR__.DS.'libraries'.DS.'file-auth.php',
 ]);
 
-Auth::extend('file', function () {
+System\Auth::extend('file', function () {
     return new Esyede\FileAuth();
 });
